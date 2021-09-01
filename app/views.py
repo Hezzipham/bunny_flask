052315fb2efeac84f3e95f24a288ffe9be93bf56
Hezzi Pham <52872927+Hezzipham@users.noreply.github.com>
2021-08-31 21:50:25 -0400

    da

--- app/views.py
+++ app/views.py
@@ -38,6 +38,7 @@ def blog():
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     # API_endpoint = "https://api.sheety.co/6da288b8b48fbffb4c7b5ec0afb715c8/questions/formResponses1"
     # sheety_response = requests.get(API_endpoint)
     # rows = sheety_response.json()["formResponses1"]
@@ -50,11 +51,16 @@ def blog():
 >>>>>>> parent of fd72245 (data)
     return render_template('blog.html')
 =======
+=======
+>>>>>>> parent of 3c2a009 (remove request)
     API_endpoint = "https://api.sheety.co/6da288b8b48fbffb4c7b5ec0afb715c8/questions/formResponses1"
     sheety_response = requests.get(API_endpoint)
     rows = sheety_response.json()["formResponses1"]
     q_list = [(row["id"] -1,row["timestamp"], row["câuHỏi/LờiNhắnYourQuestion/Message"]) for row in rows]
     return render_template('blog.html', q_list = q_list )
+<<<<<<< HEAD
+>>>>>>> parent of 3c2a009 (remove request)
+=======
 >>>>>>> parent of 3c2a009 (remove request)
 
 
